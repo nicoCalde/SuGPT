8a42b3f1e3d2739e9e00e7e70c230d772b4f57a9
nicoCalde <nicogonzalezcalderon@gmail.com>
2023-07-24 14:54:18 -0300

    api key

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
 
-const API_KEY = `process.env.REACT_APP_API_KEY`;
+const API_KEY = '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';
 
 function App() {
   const [typing, setTyping] = useState(false);
--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/SuGPT/",
-  define: {
-    'process.env.REACT_APP_API_KEY': JSON.stringify(import.meta.env.VITE_SOME_KEY),
-  },
+  base: "/SuGPT/"
 });
