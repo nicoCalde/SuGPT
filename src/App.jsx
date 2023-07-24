01fc83f13ca89f64f8cba5bdf08e2375f7b65d2a
nicoCalde <nicogonzalezcalderon@gmail.com>
2023-07-24 13:29:45 -0300

    trying new env var call

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
 
-const API_KEY = process.env.VITE_SOME_KEY;
+const API_KEY = import.meta.env.VITE_SOME_KEY;
 
 function App() {
   const [typing, setTyping] = useState(false);
