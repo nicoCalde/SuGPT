36c8dbdea994450680f6f083296614d6b19e5117
nicoCalde <nicogonzalezcalderon@gmail.com>
2023-07-24 14:41:37 -0300

    trying process

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/SuGPT/"
+  base: "/SuGPT/",
+  define: {
+    'process.env': {
+      REACT_APP_API_KEY: JSON.stringify(import.meta.env.VITE_SOME_KEY),
+    },
+  },
 })
